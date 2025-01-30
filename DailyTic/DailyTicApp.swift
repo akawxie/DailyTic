b6784800ed809bb5ac72d84904c1cfce6dcf2d0c
SuperAka <xie_wenjun@wuxiapptec.com>
2025-01-31 00:58:51 +0800

    Initial Commit

---
+++ DailyTic/DailyTicApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyTicApp.swift
+//  DailyTic
+//
+//  Created by xie_wenjun on 2025/1/31.
+//
+
+import SwiftUI
+
+@main
+struct DailyTicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
